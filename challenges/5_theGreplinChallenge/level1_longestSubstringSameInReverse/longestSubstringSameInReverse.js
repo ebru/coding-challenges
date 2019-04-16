444dc5b420d27dedfecf7a79945d36aa1f9c8827
Ebru Kaya <ebky14@gmail.com>
2019-04-16 20:34:36 +0300

    code refactored

--- challenges/5_theGreplinChallenge/level1_longestSubstringSameInReverse/longestSubstringSameInReverse.js
+++ challenges/5_theGreplinChallenge/level1_longestSubstringSameInReverse/longestSubstringSameInReverse.js
@@ -4,35 +4,33 @@
  * @param {string} str 
  */
 function longestSubstringSameInReverse(str) {
-    let longest = 0;
-    let longestStr = '';
-  
-    function isSameInReverse(str) {
-      for (let i = 0; i < Math.floor(str.length / 2); i++) {
-        if (str[i] !== str[str.length - i - 1]) {
-          return false;
-        }
-      }
-  
-      return true;
-    }
-  
-    for (let i = 0; i < str.length; i++) {
-      for (let j = 1; j < str.length; j++) {
-  
-        let substr = str.substring(i, j);
-  
-        if (isSameInReverse(substr)) {
-          if (substr.length > longest) {
-            longest = substr.length;
-            longestStr = substr;
-          }
-        }
-  
+  let longest = '';
+
+  function isSameInReverse(str) {
+    for (let i = 0; i < Math.floor(str.length / 2); i++) {
+      if (str[i] !== str[str.length - i - 1]) {
+        return false;
       }
     }
-  
-    return longestStr;
+
+    return true;
   }
 
-  module.exports = longestSubstringSameInReverse;
+  for (let i = 0; i < str.length; i++) {
+    for (let j = 1; j < str.length; j++) {
+
+      let substr = str.substring(i, j);
+
+      if (isSameInReverse(substr)) {
+        if (substr.length > longest.length) {
+          longest = substr;
+        }
+      }
+
+    }
+  }
+
+  return longest;
+}
+
+module.exports = longestSubstringSameInReverse;
